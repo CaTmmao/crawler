dba9a7e16654a59723d3109658e00b115268193b
catmmao <catmmao@gmail.com>
2020-11-07 12:34:28 +0800

    refactor:项目结构修改

---
+++ src/main/java/com/github/catmmao/Main.java
@@ -0,0 +1,7 @@
+package com.github.catmmao;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/github/catmmao/crawler/Crawler.java
+++
@@ -1,7 +0,0 @@
-package com.github.catmmao.crawler;
-
-public class Crawler {
-    public static void main(String[] args) {
-
-    }
-}
